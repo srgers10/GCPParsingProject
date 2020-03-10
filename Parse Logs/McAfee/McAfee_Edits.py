9442f7424c0a0da0397f672ec3bcf2d2f226b056
Bryce Funk <bryce.conner.funk@gmail.com>
2020-03-10 14:34:07 -0500

    Creating a normilization script for McAfee data

--- Parse Logs/McAfee/McAfee_Edits.py
+++ Parse Logs/McAfee/McAfee_Edits.py
@@ -1,7 +1,33 @@
 import pandas as pd
 
-df=pd.read_excel("McAfee_Raw_Events_edit_002.xlsx")
+## Read in the excel with raw event logs
 
-df.replace(to_replace=r'\w{4}\_\w{2}\=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
-           value='dest_ip= 123.45.67.891', regex=True)
+df=pd.read_excel("McAfee_Raw_Events_edit_003.xlsx")
 
+## Replace the dest_ip field using regex
+
+df=df.replace(to_replace=r'\w{4}\_\w{2}\=.\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.',
+           value='dest_ip= 923.45.67.891', regex=True)
+
+## Replace the src_ip field using regex
+
+df=df.replace(to_replace=r'\w{3}\_\w{2}\=.\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.',
+           value='src_ip= 923.45.67.891', regex=True)
+
+## Replace the user="<number>" using regex"
+
+df=df.replace(to_replace=r'\w{4}\=\"\d{5,7}\"',
+           value='user= 8675309', regex=True)
+
+## Replace the numerics with same count as an ID with user=<number>
+
+df=df.replace(to_replace=r'\s*\d{5,7}',
+              value='8675309', regex=True)
+## Fixes multi user issue
+
+df=df.replace(to_replace=r'\w{4}\=\"\d{5,7}\,\d{5,7}\,\d{5,7}\"',
+           value='user= 8675309', regex=True)
+
+## Saves what you did. 
+
+df.to_excel("McAfee_Raw_Events_edit_003.xlsx", index=False, header=False)
---
+++ Parse Logs/McAfee/McAfee_Normalization.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from collections import defaultdict
+#-----------------------------------------------------------------------------------------
+# Initialization of the Column
+#-----------------------------------------------------------------------------------------
+data_file = "McAfee_Raw_Events.txt"
+
+data_file_delim = ', '
+column_names = set()
+
+with open(data_file, 'r') as temp_f:
+    lines = temp_f.readlines()
+
+    for l in lines:
+        temp = l.split(data_file_delim)
+        for item in temp:
+            temp_split = item.split("=")
+            if len(temp_split) >= 2:
+                column_names.add(temp_split[0])
+
+temp_f.close()
+column_names = list(column_names)
+#-----------------------------------------------------------------------------------------
+print(column_names)
+events = defaultdict(list)
+with open(data_file, 'r') as temp_f:
+    lines = temp_f.readlines()
+    
+    for l in lines:
+        temp = l.split(data_file_delim)
+        for item in temp:
+            temp_split = item.split("=")
+            if len(temp_split) >= 2:
+                if temp_split[0] in column_names:
+                    events[temp_split[0]].append(temp_split[1])
+                else:
+                    events[temp_split[0]].append("")
+
+print(events)
+##df = pd.DataFrame.from_dict(events)
+##df.to_excel("McAfeeTestNew.xlsx", index=False)
+
+        
