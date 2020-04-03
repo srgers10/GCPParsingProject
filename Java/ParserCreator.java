3411e1bb896e8c64720f81c52bbdbc5c7dfb28da
Stephen <stephen.rogers6@gmail.com>
2020-04-03 15:20:27 -0500

    Added docs for CliHelper and ParseTester

--- Java/ParserCreator.java
+++ Java/ParserCreator.java
@@ -5,58 +5,7 @@ import java.util.regex.*;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 
-/**
- * Allows the ParseTester to run with arguments to help with the automation. Takes the following arguments
- * <hr>
- * -lp 'logPath' -cp 'commandPath' -op 'outputPath' -s 'splitter'
- * <ul>
- *  <li><b>logPath. </b><i>This is the path to the file you want to parse.</i></li> 
- *  <li><b>commandPath. </b><i>This is the path to the command file which includes your regex and delimiter instructions.</i></li> 
- *  <li><b>outputPath. </b><i>This is the path where the parser will save the parsed data to JSON format</i></li> 
- *  <li><b>splitter.</b> This is the RegEx used to split the events. By default it use ([\n\r]+) which splits the file by new line and carrige returns</li> 
- *  -cp <command_path> -op <output_path> -s <splitter></ul>
- * <hr>
- * @author Stephen Rogers
- * @author Patrick Kelly
- * @author Utsav Shrestha
- */
 
-class CliHelper{
-    private String[] args = null;
-    private HashMap<String, Integer> switchIndexes = new HashMap<String, Integer>();
-    private TreeSet<Integer> takenIndexes  = new TreeSet<Integer>();
-
-    public CliHelper(String[] args) {
-        parse(args);
-    }
-
-    void parse(String[] arguments){
-        this.args = arguments;
-        //locate switches.
-        switchIndexes.clear();
-        takenIndexes.clear();
-        for(int i=0; i < args.length; i++) {
-            if(args[i].startsWith("-") ){
-                switchIndexes.put(args[i], i);
-                takenIndexes.add(i);
-            }
-        }
-    }
-
-    public String switchValue(String switchName) {
-        try{
-            int switchIndex = switchIndexes.get(switchName);
-            if(switchIndex + 1 < args.length){
-                takenIndexes.add(switchIndex +1);
-                return args[switchIndex +1];
-            }
-        }
-        catch(Exception ex){
-            return "";
-        }
-        return "";
-    }
-}
 /**
  * parses out fields within any given file, based on a given command file. Fields can be extracted by either RegEx, a Delimitter, or via an XML field
  * <hr>
